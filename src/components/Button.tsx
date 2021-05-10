8440d0550e750eace69343313f55b8d71152091d
Ben <ben.weberj@gmail.com>
2021-05-10 16:25:32 -0700

    Add icon support and fix clipping SVG border stroke.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, useContext } from 'react'
 import styled from 'styled-components'
 
 import { IProps } from '../utils'
-import { Text, Div } from './index'
+import { Context } from '../App'
+import { Text, Div, Icon } from './index'
 
 interface Props extends IProps {
-  w?: string | number;
-  h?: string | number;
+  action?: string, // intent (just don't like that word bc Luke/Omic)
   block?: boolean; // display: block
   selected?: boolean;
   noborder?: boolean; // no border + elevates on hover
@@ -20,118 +20,70 @@ interface Props extends IProps {
   i?: number, // index to determine transition delay (for an gradual displalying of group of buttons)
 }
 
-// const Btn = styled.button<Props>(props => ({
-//   ...globalProps({ p: '.4rem 2rem', ...props }),
-//   display: props.block ? 'block' : undefined,
-//   cursor: 'pointer',
-//   position: 'relative',
-//   color: props.theme.complement,
-//   outline: 0,
-//   background: props.selected ? props.theme.complement : 'none',
-//   border: props.noborder ? 'none' : `1px solid ${props.theme[props.inverse ? 'base' : 'complement']}`,
-//   fontSize: props.size || undefined,
-//   borderRadius: props.rad || 9999,
-//   fontWeight: props.theme.regular,
-//   opacity: props.silent ? '.3' : 1,
-//   overflow: 'hidden',
-//   // boxSizing: 'border-box',
-//   '& p': { transition: `all ${props.time || .25}s ease` },
-
-//   '&:hover': props.noborder ? {
-//     opacity: 1,
-//     '&:after': { transform: 'translateY(100%) scale(.7)' },
-//     '&:before': { transform: 'scaleY(1)' },
-//     '& p': { transform: 'translateY(5px)', opacity: .5 }
-//   } : {
-//     background: props.theme.complement,
-//     color: props.theme.base,
-//   }
-// }))
-
 const Rect = styled.rect<any>(props => ({
   stroke: props.theme.mode === 'dark' ? props.theme.accent : props.theme.complement,
+  strokeWidth: props.hov ? props.stroke : 0,
+  fill: (props.hov || props.action) ? `${props.theme.complement}00` : `${props.theme.complement}15`,
 }))
 
 const Button: React.FC<Props> = props => {
-  const textRef = useRef() // to calculate original text dimensions
-  const svgRef = useRef<any>()
-
   const [hov, setHov] = useState(false)
   const [on, setOn] = useState(false)
   const [len, setLen] = useState(1)
   const [dim, setDim] = useState({ w: 120, h: 20 })
+  const textRef = useRef() // to calculate original text dimensions
+  const svgRef = useRef<any>()
+  const {  } = useContext(Context)
+
+  const { i=0, children, action } = props
+
   const px = 30
   const py = 10
-  const { i=0 } = props
-  const time = .5 // animation duration
+  const time = .8 // animation duration
+  const stroke = 2
+  const { w, h } = dim
+  const ww = w + (px*2) + stroke
+  const hh = h + (py*2) + stroke
+
+  const pathStyles = {
+    style: { transition: `all ${time}s ease` },
+    strokeDasharray: len,
+    strokeDashoffset: hov ? 0 : len,
+    on: true,
+    hov,
+  }
   
-  
-  useEffect(() => { setTimeout(() => setOn(true), (i*500)) }, [])
-  
+  useEffect(() => {
+    setTimeout(() => setOn(true), (i*500))
+  }, [])
+
   useEffect(() => {
     const tRef: any = textRef?.current
     const sRef: any = svgRef?.current
-    // console.log(el)
-    // setDim(dim)
     const n = { w: tRef.clientWidth, h: tRef.clientHeight }
-    if (Math.abs(w - n.w) > 2 || Math.abs(h - n.h) > 2) {
-      setDim({ w: n.w, h: n.h })
-    }
+    
+    if (Math.abs(w - n.w) > 2 || Math.abs(h - n.h) > 2) setDim({ w: n.w, h: n.h })
     if (sRef) setLen(sRef.getTotalLength())
   })
 
-  useEffect(() => {
-    console.log(`${props.children} --> ${len.toFixed()}px`)
-  }, [len])
-
-  // useEffect(() => {
-  //   setOn(!hov)
-  // }, [hov])
-  
-  const pathStyles = {
-    style: {
-      transition: `all ${time}s ease`,
-      transform: hov && 'translateY(100%) scaleY(.1)',
-    },
-    strokeWidth: on && !hov ? 1 : 0,
-    fill: hov ? '#000' : '#0000',
-
-    // opacity: on ? 1 : 0,
-    strokeDasharray: len,
-    strokeDashoffset: on && !hov ? 0 : len,
-  }
-
-  const { w, h } = dim
-  const ww = w + (px*2)
-  const hh = h + (py*2)
-  // const dot = hov ? 8 : 3
+  const content = action ? <>
+    <Div h={hh} full center ty={hov ? -hh-(stroke/2) : 0}><Icon name={action} /></Div>
+    <Div h={hh} full center ty={hov ? -hh-(stroke/2) : 0}><Text>{children}</Text></Div>
+  </> : <Div h={hh} full center><Text>{children}</Text></Div>
 
   return <>
     {/* hidden reference text */}
-    <Text ref={textRef} locked o={'0'} style={{ position: 'absolute' }}>{props.children}</Text>
+    <Text nowrap ref={textRef} disabled o={'0'} style={{ position: 'absolute' }}>{props.children}</Text>
 
-    {/* <Div style={{ position: 'absolute', transform: `translate(${((props.i||1)-1)*270}px, -300px)` }}>
-      <Text type='h3'>Props</Text>
-      {Object.keys(props).map(key => <Text bold>{key}: <Text inline>{props[key]}</Text></Text>)}
-
-      <Text bold>on: <Text inline>{on.toString()}</Text></Text>
-      <Text bold>len: <Text inline>{len}</Text></Text>
-      <Text bold>w -- ww: <Text inline>{w} -- {ww}</Text></Text>
-      <Text bold>h -- hh: <Text inline>{h} -- {hh}</Text></Text>
-      <Text bold>dim: <Text inline>{JSON.stringify(dim)}</Text></Text>
-    </Div> */}
-
-    <Div rel w={ww} h={hh} minW={ww} minH={hh} {...props} onClick={() => setOn(!on)} onMouseOver={() => setHov(true)} onMouseOut={() => setHov(false)}>
-      {/* <Div abs w={dot} h={dot} rounded bg='#fff3' l='50%' tx={'-50%'} ty={hov ? '-300%' : 0} o={hov ? 1 : '0'} time={time*.8} /> */}
-      <svg width={ww} height={hh} style={{ filter: `blur(${hov ? 12 : 0}px)`, transition: `all ${time}s ease` }}>
-        <Rect ref={svgRef} width={ww} height={hh} rx={hh/2} ry={hh/2} {...pathStyles} />
+    <Div active={{ transform: 'scale(.95)' }} time={.1} rel w={ww+stroke/2} h={hh+stroke/2} minW={ww} minH={hh} {...props} onClick={() => setOn(!on)} onMouseOver={() => setHov(true)} onMouseOut={() => setHov(false)}>
+      <svg width={ww} height={hh} style={{ transition: `all ${time}s ease` }} viewBox={`0 0 ${ww+stroke/2} ${hh+stroke/2}`}>
+        <Rect x={stroke/2} y={stroke/2} stroke={stroke} ref={svgRef} width={ww} height={hh} rx={hh/2} ry={hh/2} {...pathStyles} action={action} />
       </svg>
-      <Text nowrap light locked center time={time*.8} lh={`${hh}px`} ty={-hh-2 - (hov?10:0)} o={on ? 1 : .2}>
-        {props.children}
-      </Text>
+      <Div full circle contain h={hh} ty={-hh-2} style={{ userSelect: 'none' }}>
+        {content}
+      </Div>
     </Div>
   </>
-  // return <Btn {...props}><p>{props.children}</p></Btn>
 }
 
 export default Button
